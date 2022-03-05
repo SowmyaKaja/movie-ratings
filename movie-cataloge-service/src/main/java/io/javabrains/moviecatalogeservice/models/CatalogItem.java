0970a3ff7ab222bbfd5d457859a0ba7ad4eed1d6
KSowmya <kaja.sowmya10@gmail.com>
2022-03-04 23:21:06 -0600

    dd

--- movie-cataloge-service/src/main/java/io/javabrains/moviecatalogeservice/models/CatalogItem.java
+++ movie-cataloge-service/src/main/java/io/javabrains/moviecatalogeservice/models/CatalogItem.java
@@ -3,9 +3,9 @@ package io.javabrains.moviecatalogeservice.models;
 public class CatalogItem {
     private String movie_name;
     private String movie_desc;
-    private String movie_rating;
+    private int movie_rating;
 
-    public CatalogItem(String movie_name, String movie_desc, String movie_rating) {
+    public CatalogItem(String movie_name, String movie_desc, int movie_rating) {
         this.movie_name = movie_name;
         this.movie_desc = movie_desc;
         this.movie_rating = movie_rating;
@@ -27,11 +27,11 @@ public class CatalogItem {
         this.movie_desc = movie_desc;
     }
 
-    public String getMovie_rating() {
+    public int getMovie_rating() {
         return movie_rating;
     }
 
-    public void setMovie_rating(String movie_rating) {
+    public void setMovie_rating(int movie_rating) {
         this.movie_rating = movie_rating;
     }
 }
