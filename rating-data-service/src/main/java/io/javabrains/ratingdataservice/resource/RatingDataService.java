93b649ffbe42c84778ec0062e701a3c5b8a7be5c
KSowmya <kaja.sowmya10@gmail.com>
2022-03-05 00:45:48 -0600

    -making the API call using Rest Template for each rating, replace it with the catalog item - connected movie info service using the getForObj(),that takes two arguments url and movie information and returns a string.

--- rating-data-service/src/main/java/io/javabrains/ratingdataservice/resource/RatingDataService.java
+++ rating-data-service/src/main/java/io/javabrains/ratingdataservice/resource/RatingDataService.java
@@ -1,9 +1,13 @@
 package io.javabrains.ratingdataservice.resource;
 
 import io.javabrains.ratingdataservice.models.Rating;
+import io.javabrains.ratingdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @RestController
@@ -12,7 +16,18 @@ public class RatingDataService {
     @RequestMapping("/{movieId}")
     public Rating getRatingData(@PathVariable ("movieId") String movieId) {
         return new Rating(movieId, 5);
+    }
 
-
+    @RequestMapping("users/{userId}")
+    public UserRating getUserRating(@PathVariable ("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 3),
+                new Rating("2344",5),
+                new Rating("3456",4),
+                new Rating("5555", 3)
+        );
+       UserRating userRating =  new UserRating();
+        userRating.setGetUserRatings(ratings);
+        return userRating;
     }
 }
