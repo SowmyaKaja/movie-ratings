bdbee90fc243ab7862dfdeeeb8fa1c3b625ca8cc
KSowmya <kaja.sowmya10@gmail.com>
2022-03-05 00:48:41 -0600

    -making the API call using Rest Template for each rating, replace it with the catalog item - connected movie info service using the getForObj(),that takes two arguments url and movie information and returns a string.

--- movie-cataloge-service/src/main/java/io/javabrains/moviecatalogeservice/Resource/MovieCatalogResource.java
+++ movie-cataloge-service/src/main/java/io/javabrains/moviecatalogeservice/Resource/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package io.javabrains.moviecatalogeservice.Resource;
 import io.javabrains.moviecatalogeservice.models.CatalogItem;
 import io.javabrains.moviecatalogeservice.models.Movie;
 import io.javabrains.moviecatalogeservice.models.Rating;
+import io.javabrains.moviecatalogeservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,25 +31,24 @@ public class MovieCatalogResource {
     @RequestMapping("/{userID}")
     public List<CatalogItem> getCatalog(@PathVariable("userID") String userID) {
         //get all rated movies ID's
-    List<Rating> ratings = Arrays.asList(
-            new Rating("1234", 3),
-            new Rating("2344",5),
-            new Rating("3456",4),
-            new Rating("5555", 3)
-            );
-    // making the API call using Rest Template
-    // for each rating, replace it with the catalog item.
+    UserRating ratings =restTemplate.getForObject("http://localhost:8082/ratingdata/users/" + userID, UserRating.class);
 
-    return ratings.stream().map(rating -> {
-        // --------------restTemplate---------------------
-        // getForObj, take two arguments url and movie information and returns a string.
+
+    return ratings.getGetUserRatings().stream().map(rating -> {
+        // --------------restTemplate--------------------
+        // for each movie ID call the movie info service and get details
         // making a call to the API and unmarshall the object.
         Movie movie = restTemplate.getForObject("http://localhost:8081/movie/" + rating.getMovieId(),
                 // need ,movie id n movie name we can get that from movie class
                 // by copy-paste the class in mcr it.
                 Movie.class);
+// put them all together.
+        return new CatalogItem(movie.getName(), "very good", rating.getRating());
+    }).collect(Collectors.toList());//to get the information of catalog items list and returning themcd
+    }
+}
 
-      /* -------------WebClientBuilder  ----------------------
+/* -------------WebClientBuilder  ----------------------
        //way to use the web client. Calling the movie using the webClient.builder
         Movie movie = webClientBuilder.build()
                 .get()
@@ -56,13 +56,3 @@ public class MovieCatalogResource {
                 .retrieve()
                 .bodyToMono(Movie.class)
                 .block();*/
-
-        return new CatalogItem(movie.getName(), "very good", rating.getRating());
-    }).collect(Collectors.toList());//to get the information of catalog items list and returning themcd
-
-    // for each ID, call Movie info service and get details
-
-    // put them all together.
-
-    }
-}
