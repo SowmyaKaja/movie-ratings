a7ba4a9ef7c6a4c4a814fc9dd13d209279bdf198
KSowmya <kaja.sowmya10@gmail.com>
2022-03-04 23:20:50 -0600

    dd

--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/resource/MovieInfoService.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/resource/MovieInfoService.java
@@ -10,6 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieInfoService {
 @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable ("movieId") String movieId) {
+    if (movieId.equalsIgnoreCase("1234")) {
+        return new Movie(movieId, "Interstellar");
+    } else if (movieId.equalsIgnoreCase("2344")) {
+        return new Movie(movieId, "Rab ne Bana di Jodi");
+    } else if (movieId.equalsIgnoreCase("3456")) {
+        return new Movie(movieId, "Gopala Gopala");
+    } else {
         return new Movie(movieId, "Aarya");
     }
+}
 }
