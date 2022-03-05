2612ee642decad5713ebe99f2cd176e234f47156
KSowmya <kaja.sowmya10@gmail.com>
2022-03-05 00:53:33 -0600

    -making the API call using Rest Template for each rating, replace it with the catalog item - connected movie info service using the getForObj(),that takes two arguments url and movie information and returns a string.

---
+++ movie-cataloge-service/src/main/java/io/javabrains/moviecatalogeservice/models/UserRating.java
@@ -0,0 +1,15 @@
+package io.javabrains.moviecatalogeservice.models;
+
+import java.util.List;
+
+public class UserRating {
+    public List<Rating> getGetUserRatings() {
+        return getUserRatings;
+    }
+
+    public void setGetUserRatings(List<Rating> getUserRatings) {
+        this.getUserRatings = getUserRatings;
+    }
+
+    private List<Rating> getUserRatings;
+}
